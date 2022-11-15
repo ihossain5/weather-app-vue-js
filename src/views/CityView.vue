e1c878df29ecf8821db6f5f04d62383ccc4e8beb
ihossain5 <ismail.hossain5169@gmail.com>
2022-11-15 12:22:12 +0600

    add city wise weather details

---
+++ src/views/CityView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <Suspense>
+            <AsyncCityView></AsyncCityView>
+        </Suspense>
+    </div>
+</template>
+
+<script setup>
+import AsyncCityView from '../components/AsyncCityView.vue';
+
+</script>
